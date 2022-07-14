3a13fbc28ddda28575dcf8b8c01e2f2c32af9213
平本 光 <200160516@anabuki-net.ne.jp>
2022-07-14 09:41:02 +0900

    a

---
+++ 開発用/GameProgramming/CBossStage.cpp
@@ -0,0 +1,41 @@
+#include"CCollisionManager.h"
+#include"CColliderTriangle.h"
+#include"CBossStage.h"
+#include"CCollider.h"
+#define OBJ "Resource\\3DModel\\BossStage\\BossStage.obj"
+#define MTL "Resource\\3DModel\\BossStage\\BossStage.mtl"
+
+CModel CBossStage::mModel;//モデルデータ作成
+
+CBossStage::CBossStage(const CVector& position, const CVector& rotation, const CVector& scale)
+
+{
+
+	mPosition = position;
+	mRotation = rotation;
+	mScale = scale;
+
+	//優先度を2に変更する
+	mPriority = 2;
+	CTaskManager::Get()->Remove(this);//削除して
+	CTaskManager::Get()->Add(this);//追加する
+	CTransform::Update();//行列の更新
+
+   //モデルのポインタ設定
+	mpModel = &mModel;
+	//モデルが無いときは読み込む
+	if (mModel.mTriangles.size() == 0) {
+		mModel.Load(OBJ, MTL);
+	}
+	mColliderMesh.Set(this, &mMatrix, &mModel);//モデルをコライダにする
+	mTag = EBOSSSTAGE;
+}
+void CBossStage::Update() {
+	//CCharacterの更新
+	CTransform::Update();
+}
+
+void CBossStage::Render() {
+	//親の描画処理
+	CCharacter::Render();
+}
---
+++ 開発用/GameProgramming/CBossStage.h
@@ -0,0 +1,22 @@
+#include"CCollider.h"
+#include"CModel.h"
+#include"CCharacter.h"
+#include"CColliderMesh.h"
+#ifndef CBOSSSTAGE_H
+#define CBOSSSTAGE_H
+class CBossStage : public CCharacter {
+private:
+
+public:
+	CColliderMesh mColliderMesh;//岩の３Dモデルのコライダ
+	//モデルデータ
+	static CModel mModel;
+	//コライダ
+	CBossStage(const CVector& position, const CVector& rotation, const CVector& scale);
+	//更新処理
+	void Update();
+	//衝突処理
+
+	void Render();
+};
+#endif
